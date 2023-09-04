6fb75b5ab64e0dd19d5f5c86c245c8ef27adc8d1
dcbuilder.eth <dcbuilder@protonmail.com>
2023-09-04 11:35:54 +0100

    finish week 4

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -4,9 +4,28 @@
 // File name: projects/04/Mult.asm
 
 // Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-//
-// This program only needs to handle arguments that satisfy
-// R0 >= 0, R1 >= 0, and R0*R1 < 32768.
+// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
-// Put your code here.
+    @2
+    M=0     // R2 = 0
+    @i
+    M=0     // i=0
+(LOOP)
+    @i
+    D=M     // D=i
+    @0
+    D=D-M   // D=i-R0
+    @END
+    D;JGE    // if i-R0 >= 0 goto END
+
+    @1
+    D=M     // D=R1
+    @2
+    M=D+M   // R2=R2+R1
+    @i
+    M=M+1   // i=i+1
+    @LOOP
+    0;JMP   // Repeat
+(END)
+    @END
+    0;JMP
